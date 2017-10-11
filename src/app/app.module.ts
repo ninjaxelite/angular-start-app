c908f6e91d5d0001f5b18ed5d7d2c1c724cf2b78
Magomed Arsaev <magamet@hotmail.de>
2017-10-11 12:02:16 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)] ],
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
+  ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  bootstrap:    [ AppComponent ],
+  providers:    [ HeroService ]
+})
+export class AppModule { }
