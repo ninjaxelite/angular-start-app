52b9aca3e03938ff7e0a4c31afbfc0e3e128a645
Magomed Arsaev <magamet@hotmail.de>
2017-10-13 15:39:33 +0200

    database connection with postgres db established

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const pg = require('pg'); //require('pg-promise')(/*options*/)
+const dbUrl = 'pg://postgres:postgres@localhost:5432/angularDB';
+const client = new pg.Client(dbUrl);
+client.connect();
+
 const heroes = [
     { id: 0,  name: 'Zero' },
     { id: 11, name: 'Mr. Nice' },
@@ -15,6 +20,35 @@ const heroes = [
     { id: 20, name: 'Tornado' }
   ];
 
+
+  //db.query('INSERT INTO $1~($2~) VALUES($3~,$4~)', ['heroes', 'heroname', '1', 'myhero']);
+
+  client.query("INSERT INTO heroes(id_pk,heroname) values($1,$2)", ['3','myherooo']);
+
+ /* pg.connect(dbUrl, function(err, client, done) {
+    var i = 0, count = 0; 
+    for (i = 0; i < 1000; i++) {
+        client.query(
+            'INSERT into heroes VALUES($2) RETURNING id', 
+            ['myherooo'], 
+            function(err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('row inserted with id: ' + result.rows[0].id);
+                }
+
+                count++;
+                console.log('count = ' + count);
+                if (count == 1000) {
+                    console.log('Client will end now!!!');
+                    client.end();
+                }
+            });        
+    }
+});*/
+
+
 router.get('/heroes', (req, res) => {
   res.send(JSON.stringify(heroes));
 });
@@ -24,7 +58,7 @@ router.get('/heroes/hero/:id', (req, res) => {
 });
 
 router.get('/heroes/hero/update/:id', (req, res) => {
-  var h = req.params.id;
+  let h = req.params.id;
   console.info("save: "+h);
   res.send(JSON.stringify("ok"));
 });
